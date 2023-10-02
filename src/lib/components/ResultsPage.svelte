48d8f22095d906acbbf0a69f9eb86ca2e0bfa95c
Broscaru Sorin Mihai <broscaru.sorin@gmail.com>
2023-10-02 11:47:39 +0300

    Add basic viewporting for infinte loading

--- src/lib/components/ResultsPage.svelte
+++ src/lib/components/ResultsPage.svelte
@@ -1,34 +1,99 @@
 <script lang="ts">
-	import { media } from "$lib/api";
-	import type { MovieListResult } from "$lib/types";
+	import { media } from '$lib/api';
+	import type { MovieListResult } from '$lib/types';
+	import { createEventDispatcher, onMount } from 'svelte';
 
-    export let movies: MovieListResult[];
-    export let next: string | null;
+	export let movies: MovieListResult[];
+	export let next: string | null;
+
+    const dispatch = createEventDispatcher();
+
+	let viewport: HTMLDivElement;
+	let results: HTMLDivElement;
+	let item_width: number;
+	let item_height: number;
+	let num_columns = 4;
+
+	let a = 0;
+	let b = movies.length;
+	let padding_top = 0;
+	let padding_bottom = 0;
+
+	function handle_resize() {
+		const first = results.firstChild as HTMLAnchorElement;
+
+		item_width = first.offsetWidth;
+		item_height = first.offsetHeight;
+
+		num_columns = 4;
+		handle_scroll();
+	}
+
+	function handle_scroll() {
+        const remaining = viewport.scrollHeight - (viewport.scrollTop + viewport.clientHeight);
+
+        if(remaining < 400) {
+            dispatch('end')
+        }
+
+		a = Math.floor(viewport.scrollTop / item_height) * num_columns;
+		b = Math.ceil((viewport.scrollTop + viewport.clientHeight) / item_height) * num_columns;
+
+		padding_top = Math.floor(a / num_columns) * item_height;
+		padding_bottom = Math.floor((movies.length - b) / num_columns) * item_height;
+
+        console.log(remaining);
+	}
+
+	onMount(handle_resize);
 </script>
 
-<div class="results">
-    {#each movies as movie}
-        <a href="/movies/{movie.id}">
-            <img src={media(movie.poster_path, 500)} alt={movie.title}>
-        </a>
-    {/each}
+<svelte:window on:resize={handle_resize} />
+
+<p>showing items {a} to {b}</p>
+
+<div class="viewport" bind:this={viewport} on:scroll={handle_scroll}>
+	<div
+		class="results"
+		bind:this={results}
+		style:padding-top="{padding_top}px"
+		style:padding-bottom="{padding_bottom}px"
+	>
+		{#each movies.slice(a, b) as movie}
+			<a href="/movies/{movie.id}">
+				<img src={media(movie.poster_path, 500)} alt={movie.title} />
+			</a>
+		{/each}
+	</div>
+	{#if next}
+		<a href={next}>next page</a>
+	{/if}
 </div>
 
 <style>
-    .results {
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        width: 100%;
-    }
+	.viewport {
+		overflow-y: auto;
+		height: 500px;
+	}
 
-    a {
-        width: 100%;
-        height: auto;
-        padding: 0.5rem;
-        aspect-ratio: 2/3;
+    .viewport::-webkit-scrollbar {
+        display: none;
     }
+    
+	.results {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		width: 100%;
+	}
 
-    img {
-        width: 100%;
-    }
+	a {
+		width: 100%;
+		height: auto;
+		padding: 0.5rem;
+		aspect-ratio: 2/3;
+	}
+
+	img {
+		width: 100%;
+	}
 </style>
--- src/routes/movies/[view=list]/+page.svelte
+++ src/routes/movies/[view=list]/+page.svelte
@@ -6,5 +6,8 @@
 
 <div class="column">
     <h1>{data.title}</h1>
-    <ResultsPage movies={data.movies} next={data.next_page ? `/movies/{data.view}?page={data.next_page}` : null } />
+    <ResultsPage movies={data.movies} next={data.next_page ? `/movies/{data.view}?page={data.next_page}` : null }
+    on:end={() => {
+        console.log('load more data');
+    }} />
 </div>
