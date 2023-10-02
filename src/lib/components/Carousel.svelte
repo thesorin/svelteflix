8d97b665d12c3652494e2221e419144ae53acebf
Broscaru Sorin Mihai <broscaru.sorin@gmail.com>
2023-10-02 10:36:00 +0300

    Add Carousel component

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { media } from '$lib/api';
+	import type { MovieListResult } from "$lib/types";
+
+    export let movies: MovieListResult[];
+</script>
+
+<div class="carousel">
+    {#each movies as movie}
+         <a href="/movie/{movie.id}">
+        <img src={media(movie.poster_path, 500)} alt={movie.title}></a>
+    {/each}
+</div>
+
+<style>
+    .carousel {
+		--padding: max(var(--side), calc(var(--side) + (100vw - var(--column)) / 2));
+		display: flex;
+		height: 10rem;
+		gap: 1rem;
+		overflow-x: scroll;
+        scroll-snap-type: x mandatory;
+        scroll-padding-left: var(--padding);
+        padding: 0 var(--padding);
+	}
+
+    .carousel::-webkit-scrollbar {
+        display: none;
+    }
+
+    a {
+        height: 100%;
+    }
+
+    img {
+        height: 100%;
+    }
+</style>
