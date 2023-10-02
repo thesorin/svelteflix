4312eaebb669a859b5ec108cf1dedf7fabba18ad
Broscaru Sorin Mihai <broscaru.sorin@gmail.com>
2023-10-02 12:12:24 +0300

    Implement caching

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,12 +3,13 @@ import type { MovieDetails, MovieList } from '$lib/types';
 import type { LoadEvent } from '@sveltejs/kit';
 
 const base = `https://api.movies.tastejs.com`;
-
+const page = '1';
+const defaultQuery = { page };
 export async function load({ fetch }: LoadEvent) {
     const [trending, now_playing, upcoming] = await Promise.all([
-        api.get(fetch, `/trending/movie/day`),
-        api.get(fetch, `/movie/now_playing`),
-        api.get(fetch, `/movie/upcoming`)
+        api.get(fetch, `/trending/movie/day`, defaultQuery),
+        api.get(fetch, `/movie/now_playing`, defaultQuery),
+        api.get(fetch, `/movie/upcoming`, defaultQuery)
     ]) as Array<MovieList>;
 
     const featured = await api.get(fetch, `/movie/${trending.results[0].id}`, {
--- src/routes/movies/[view=list]/+page.ts
+++ src/routes/movies/[view=list]/+page.ts
@@ -15,6 +15,7 @@ export async function load({ params, url, fetch }: LoadEvent<{view: string}>) {
         ...view,
         view: params.view,
         movies: data.results,
-        next_page: data.page < data.total_pages ? data.page + 1 : null
+        next_page: data.page < data.total_pages ? data.page + 1 : null,
+        infinite: true
     }
 }
